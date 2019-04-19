8943eff76a3650c7a496a670f6ba11d367c48eac
David E Weaver <dweaver309@gmail.com>
2019-04-19 13:12:08 -0500

    Add files via upload

---
+++ Pins.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Pins
+{
+
+    public static class ESP32Device
+    {
+        public static class SP1
+        {
+            public const string Name = "SPI1";
+            public const int MOSI = 23;
+            public const int MISO = 25;
+            public const int SCK = 19;
+
+        }
+
+        public static class Serial
+        {
+            public static class COM1
+            {
+                public const string Name = "COM1";
+                public const int TX = 1;
+                public const int RX = 3;
+                         
+            }
+
+            public static class COM2
+            {
+                public const string Name = "COM2";
+                public const int TX = 10;
+                public const int RX = 9;
+
+            }
+
+            public static class COM3
+            {
+                public const string Name = "COM3";
+                public const int TX = 17;
+                public const int RX = 16;
+
+            }
+
+        }
+        public static class I2C
+        {
+            public static class I2C1
+            {
+                public const string Name = "I2C1";
+                public const int DATA = 18;
+                public const int ClOCK = 19;
+             
+            }
+
+            public static class I2C2
+            {
+                public const string Name = "I2C2";
+                public const int DATA = 25;
+                public const int ClOCK = 26;
+
+            }
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+using Pins;
+
+
+namespace ESP32_WROOM_32_PINS
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("==============================================================");
+            Console.WriteLine("            ESP32 Wroom 32 default mapped pins");
+            Console.WriteLine("==============================================================" + "\n\n");
+
+
+            // SPI 
+
+            Console.WriteLine("SPI Device" + "\n");
+
+            Console.WriteLine("SPI device name =  " + ESP32Device.SP1.Name);
+
+            Console.WriteLine("MOSI pin =  " + ESP32Device.SP1.MOSI);
+
+            Console.WriteLine("MISO pin =  " + ESP32Device.SP1.MISO);
+
+            Console.WriteLine("SLK pin = " + ESP32Device.SP1.SCK + "\n\n");
+
+
+
+            // Serial
+
+            Console.WriteLine("Serial Device" + "\n");
+
+            // COM1
+
+            Console.WriteLine("Serial COM1 device name = " + ESP32Device.Serial.COM1.Name);
+
+            Console.WriteLine("Serial RX pin = " + ESP32Device.Serial.COM1.RX);
+
+            Console.WriteLine("Serial TX pin = " + ESP32Device.Serial.COM1.TX + "\n");
+
+            // COM2
+
+            Console.WriteLine("Serial COM2 device name = " + ESP32Device.Serial.COM2.Name);
+
+            Console.WriteLine("Serial RX pin = " + ESP32Device.Serial.COM2.RX);
+
+            Console.WriteLine("Serial TX pin = " + ESP32Device.Serial.COM2.TX + "\n");
+
+            // COM3
+
+            Console.WriteLine("Serial COM2 device name = " + ESP32Device.Serial.COM3.Name);
+
+            Console.WriteLine("Serial RX pin = " + ESP32Device.Serial.COM3.RX);
+
+            Console.WriteLine("Serial TX pin = " + ESP32Device.Serial.COM3.TX + "\n\n");
+
+            // I2C
+
+            Console.WriteLine("I2C Device" + "\n");
+
+            // I2C1
+
+            Console.WriteLine("I2C I2C1 device name = " + ESP32Device.I2C.I2C1.Name);
+
+            Console.WriteLine("I2C Data pin = " + ESP32Device.I2C.I2C1.DATA);
+
+            Console.WriteLine("I2C Clock pin = " + ESP32Device.I2C.I2C1.ClOCK + "\n");
+
+            // I2C2
+
+            Console.WriteLine("I2C I2C2 device name = " + ESP32Device.I2C.I2C2.Name);
+
+            Console.WriteLine("I2C Data pin = " + ESP32Device.I2C.I2C2.DATA);
+
+            Console.WriteLine("I2C Clock pin = " + ESP32Device.I2C.I2C2.ClOCK);
+                                                
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
